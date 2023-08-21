776a7f6aa1fc8c28da5df5ae376f71eae0e6582a
Raph <20ik061@esisalama.org>
2023-08-21 11:40:04 +0200

    amlioration de l'architecture

--- config.php
+++ config.php
@@ -8,16 +8,19 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 define('HOST', 'http://'.$host.'/archiveResult/');
 define('ROOT', $root.'/archiveResult/');
 
-define('CONTROLLER_ADMIN', ROOT.'controllers/Admin.php');
-define('MODEL_ADMIN', ROOT.'models/Admin.php');
-define('VIEW_ADMIN', ROOT.'views/admin/');
+//adresse absolue vers les fichiers app (controllers, models, views) 
+define('CONTROLLER_ADMIN', ROOT.'app/controllers/Admin.php');
+define('MODEL_ADMIN', ROOT.'app/models/Admin.php');
+define('VIEW_ADMIN', ROOT.'app/views/admin/');
 
-define('CONTROLLER_BULLETIN', ROOT.'controllers/Bulletin.php');
+define('CONTROLLER_BULLETIN', ROOT.'app/controllers/Bulletin.php');
 
-define('CONTROLLER_ETUDIANT', ROOT.'controllers/Etudiant.php');
-define('MODEL_ETUDIANT', ROOT.'models/Etudiant.php');
-define('VIEW_ETUDIANT', ROOT.'views/admin/');
+define('CONTROLLER_ETUDIANT', ROOT.'app/controllers/Etudiant.php');
+define('MODEL_ETUDIANT', ROOT.'app/models/Etudiant.php');
+define('VIEW_ETUDIANT', ROOT.'app/views/admin/');
 
+//adresse absolue vers routeur
 define('ROUTEUR', ROOT.'routeur/routeur.php');
 
+//lien absolue vers les assets (css, js, img)
 define('ASSET', HOST.'assets/');
