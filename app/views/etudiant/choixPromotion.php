2449b012bda7883fca0f32443daf8677551f557a
olivier kasongo <olivierkasongo539@gmail.com>
2023-09-20 08:55:41 +0200

    mise en forme de la page choix promotion

--- app/views/etudiant/choixPromotion.php
+++ app/views/etudiant/choixPromotion.php
@@ -1,16 +1,25 @@
 <?php
 $title = 'choix promotion';
 $style = ASSETS_CSS.'etudiant/choixPromotion.css';
+$bootstrap = ASSETS_BOOTSTRAP;
 require_once(HEADER);
 
 if(isset($notif))
     echo $notif;
+?>
 
+<section>
+    <p>Choix Promotion</p>
+<?php
 foreach($trouver as $promotion){
     echo '<a href="promotion?id='.$promotion["id"].'" class="'.$promotion["nom"].'">'.$promotion["nom"].'</a><br>';
 }
 ?>
+</section>
 
+<div>
+    <p>Archive Result 2023</p>
+</div>
 <?php
 require_once(FOOTER);
 ?>
